790e8af7736183b1bf454ed7bca51919154539fb
Dylan-So <31941200+Dylan-So@users.noreply.github.com>
2019-06-06 15:02:29 -0400

    Add files via upload

--- Hearthstone.java
+++ Hearthstone.java
@@ -1,33 +1,61 @@
 package hearthstone;
 
+import java.util.Scanner;
+
 public class Hearthstone{
 
     public static void main(String[] args) {
-        Puzzles[] boardClearLvls = new BoardClear[7];
-        Puzzles[] lethalLvls = new Lethal[7];
-        Puzzles[] survivalLvls = new Survival[7];
-        Puzzles[] mirrorLvls = new Mirror[7];
-        gameDeck deck = new gameDeck("C:\\Users\\324616879\\Desktop\\cards.txt");
-        for(int i = 0; i < 7; i++){
-            boardClearLvls[i] = new BoardClear();
-            lethalLvls[i] = new Lethal();
-            survivalLvls[i] = new Survival();
-            mirrorLvls[i] = new Mirror();
-        }
-        
-        
-        System.out.println(deck.card.size());
-        System.out.println(deck.card.get(0).getName());
-        System.out.println(deck.card.get(0).getHp());
-        System.out.println(deck.card.get(0).getAttack());
-        System.out.println(deck.card.get(0).getCondition());
-        System.out.println(deck.card.get(1).getName());
-        System.out.println(deck.card.get(1).getHp());
-        System.out.println(deck.card.get(1).getAttack());
-        System.out.println(deck.card.get(1).getCondition());
-        System.out.println(deck.card.get(2).getName());
-        System.out.println(deck.card.get(2).getHp());
-        System.out.println(deck.card.get(2).getAttack());
-        System.out.println(deck.card.get(2).getCondition());
+        Scanner sc = new Scanner(System.in);
+        GameDeck deck = new GameDeck("C:\\Users\\324616879\\Desktop\\cards.txt");
+        boolean win = false;
+        int optionAns = 0;
+        System.out.println("-------------Welcome to the Hearthstone Puzzle Game-------------");
+        while(optionAns != -1 || !win){
+            System.out.println("Choose from the 3 options (Enter the Number)");
+            System.out.println("1. Play Puzzles");
+            System.out.println("2. Collections");
+            System.out.println("3. Quit Game");
+            System.out.println("");
+            optionAns = sc.nextInt();
+            while(optionAns < 1 || optionAns > 3){
+                System.out.println("Please enter a valid option: ");
+                optionAns = sc.nextInt();
+            }
+            if(optionAns == 1){
+                System.out.println("Please select a puzzle you want to play");
+                System.out.println("1. Board Clear");
+                System.out.println("2. Lethal");
+                System.out.println("3. Survival");
+                System.out.println("4. Mirror");
+                int puzzleAns = 0;
+                if(puzzleAns == 1){
+                    Puzzles[] boardClearLvls = new BoardClear[7];
+                    for(int i = 0; i < 7; i++){
+                        boardClearLvls[i] = new BoardClear();
+                    }
+                    
+                }else if(puzzleAns == 2){
+                    Puzzles[] lethalLvls = new Lethal[7];
+                    for(int i = 0; i < 7; i++){
+                        lethalLvls[i] = new Lethal();
+                    }
+                    
+                }else if(puzzleAns == 3){
+                    Puzzles[] survivalLvls = new Survival[7];
+                    for(int i = 0; i < 7; i++){
+                        survivalLvls[i] = new Survival();
+                    }
+                }else{
+                    Puzzles[] mirrorLvls = new Mirror[7];
+                    for(int i = 0; i < 7; i++){
+                        mirrorLvls[i] = new Mirror();
+                    }
+                }
+            }else if(optionAns == 2){
+                
+            }else{
+                break;
+            }
+    }
     }
 }

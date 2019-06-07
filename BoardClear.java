fdb499131a42cf1a4f4af6830fbd0eec45b954f9
Dylan-So <31941200+Dylan-So@users.noreply.github.com>
2019-06-07 14:00:41 -0400

    Add files via upload

--- BoardClear.java
+++ BoardClear.java
@@ -5,6 +5,10 @@ public class BoardClear extends Puzzles {
     public BoardClear(Card[] d, Card[] ed, Card[] h, Card[] eh, Card[] fc, Card[] efc, int sm, int cm, Hero aHero, Hero eHero) {
         super(d, ed, h, eh, fc, efc, sm, cm, aHero, eHero);
     }
+    
+    public BoardClear(){
+        super(null, null, null, null, null, null, 0, 0, null, null);
+    }
 
     public boolean checkWinCondition() {//checks all enemy minions have been removed
         for(int i = 0; i < 7; i++){
